29b62c1f7761b951e58a38fa5e291ece98ec72c6
RaulOBenitez <rbenitezd93@gmail.com>
2022-05-25 20:48:41 -0400

    '#'

--- credit_card_validator.cpp
+++ credit_card_validator.cpp
@@ -25,6 +25,36 @@ int main(){
 std::string cc_type(int length, int number){
     std::string type;
     
+       string cc_type = "INVALID"; /*Setting Invalid as default output, if value doesnt change
+    it will return Invalid as no conditions were covered*/
+    if (length >= 13 && length <= 16) //validate is not smaller or larger than expected
+    {
+        int first_digits = card_number / pow(10, (length - 2)); //obtaining digit of type and verification
+        //AMEX
+        if (length == 15) //Amex validation based on length
+        {
+            if (first_digits == 34 || first_digits == 37)  //Amex validation based on first 2 digits
+            {
+                cc_type = "AMEX";
+            }
+        }
+        else if (length == 16)
+        {
+            if (first_digits >= 51 && first_digits <= 55)// Mastercad validation
+            {
+                cc_type = "MASTERCARD";
+            }
+
+            else if (first_digits >= 40 && first_digits <= 49) //VISA validation with 16 digits
+            {
+                cc_type = "VISA";
+            }
+        }
+        else if (length == 13 && first_digits >= 40 && first_digits <= 49) //VISA validation with 13 digits
+        {
+            cc_type = "VISA";
+        }
+    }
     
 
     return type;
